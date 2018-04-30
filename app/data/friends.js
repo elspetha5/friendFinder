c789a105b2109065b188e132e7c3a5507afcd4c2
elspetha5 <elspetha@gmail.com>
2018-04-30 16:52:42 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendsArray = [
+    {
+        name: "Strong Bad",
+        picture: "https://orig00.deviantart.net/de88/f/2008/277/e/8/strong_bad___payback_by_whoismarcus.gif",
+    }
+];
+
+module.exports = friendsArray;
