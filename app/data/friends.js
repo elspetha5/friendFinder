77113e230c76965b77e0eb6680d4ecec03e588c9
elspetha5 <elspetha@gmail.com>
2018-05-01 16:55:58 -0700

    finished product

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,66 @@ var friendsArray = [
     {
         name: "Strong Bad",
         picture: "https://orig00.deviantart.net/de88/f/2008/277/e/8/strong_bad___payback_by_whoismarcus.gif",
+        scores: [
+            1,
+            5,
+            2,
+            3,
+            1,
+            5,
+            5,
+            5,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Homestar Runner",
+        picture: "http://www.hrwiki.org/w/images/thumb/7/7f/Homestar%27s_Arms.PNG/180px-Homestar%27s_Arms.PNG",
+        scores: [
+            5,
+            2,
+            1,
+            1,
+            1,
+            4,
+            3,
+            2,
+            5,
+            3
+        ]
+    },
+    {
+        name: "The Cheat",
+        picture: "http://www.hrwiki.org/w/images/thumb/9/96/cheatstickynotes.PNG/150px-cheatstickynotes.PNG",
+        scores: [
+            3,
+            4,
+            5,
+            5,
+            3,
+            4,
+            2,
+            1,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Marzipan",
+        picture: "https://i.ytimg.com/vi/EtLyT6Bw0Z4/maxresdefault.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            2,
+            3,
+            2,
+            1,
+            4,
+            3,
+            5
+        ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,48 @@
 var friends = require("../data/friends");
 
 module.exports = function (app) {
-    app.get("/api/friendslist", function (req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friendslist", function (req, res) {
+    app.post("/api/friends", function (req, res) {
+        var newFriendArray = [];
+        for (var i = 0; i < req.body.scores.length; i++) {
+            var num = parseInt(req.body.scores[i]);
+            newFriendArray.push(num);
+        };
+
+        var bestMatchName = "";
+        var bestMatchPicture = "";
+
+        var bestMatchDiff = 100;
+        for (var i = 0; i < friends.length; i++) {
+
+            var totalDifference = 0;
+            var friendScore = friends[i].scores;
+
+            for (var j = 0; j < friendScore.length; j++) {
+                var num = parseInt(friendScore[j]);
+                var diff = Math.abs(num - newFriendArray[j]);
+                totalDifference = totalDifference + diff;
+            };
+
+            if (totalDifference < bestMatchDiff) {
+                bestMatchDiff = totalDifference;
+
+                bestMatchName = friends[i].name;
+                bestMatchPicture = friends[i].picture;
+            };
+        };
+
         friends.push(req.body);
-        res.json(true);
+
+        var bestMatch = {
+            name: bestMatchName,
+            picture: bestMatchPicture
+        };
+
+        res.json(bestMatch);
+        
     });
 };
